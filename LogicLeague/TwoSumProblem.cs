19e94b0fd511c21ed55123fa6dce20a79a3e6608
Sohil007 <sohil.mansuri@in.musafir.com>
2025-06-28 11:39:56 +0530

    Add project files.

---
+++ LogicLeague/TwoSumProblem.cs
@@ -0,0 +1,26 @@
+﻿namespace LogicLeague
+{
+    /// <summary>
+    /// https://leetcode.com/problems/two-sum/
+    /// </summary>
+    internal class TwoSumProblem
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> keyValuePairs = [];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (keyValuePairs.ContainsKey(diff))
+                {
+                    return [keyValuePairs[diff], i];
+                }
+
+                keyValuePairs[nums[i]] = i;
+            }
+
+            return [];
+        }
+    }
+}
