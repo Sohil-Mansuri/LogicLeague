61c6f4d2d97bb00bef5ce2d2a5152967b1e9ca92
Sohil007 <sohil.mansuri@in.musafir.com>
2025-06-28 12:11:52 +0530

    Buy sell stock problem

---
+++ LogicLeague/BuySellStock.cs
@@ -0,0 +1,34 @@
+﻿namespace LogicLeague
+{
+    /// <summary>
+    /// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    /// </summary>
+    internal class BuySellStock
+    {
+        /// <summary>
+        /// Buy when price is low, sell when price is high
+        /// </summary>
+        /// <param name="prices"></param>
+        /// <returns></returns>
+        public static int MaxProfit(int[] prices)
+        {
+            int buyPrice = prices[0];
+            int profit = 0;
+
+            foreach (var price in prices)
+            {
+                if (buyPrice > price)
+                {
+                    buyPrice = price;
+                }
+                else
+                {
+                    profit = Math.Max(profit, price - buyPrice);
+                }
+            }
+
+
+            return profit;
+        }
+    }
+}
--- LogicLeague/Program.cs
+++ LogicLeague/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            var profit = BuySellStock.MaxProfit([7, 1, 5, 3, 6, 4]);
             Console.WriteLine("Hello, World!");
         }
     }
