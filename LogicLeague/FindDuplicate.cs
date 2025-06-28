e4520c57d3e95997f7ee0183315d795da7bbe6c0
Sohil007 <sohil.mansuri@in.musafir.com>
2025-06-28 12:32:38 +0530

    contains duplicate problem

---
+++ LogicLeague/FindDuplicate.cs
@@ -0,0 +1,23 @@
+﻿namespace LogicLeague
+{
+    /// <summary>
+    /// https://leetcode.com/problems/contains-duplicate/
+    /// </summary>
+    internal class FindDuplicate
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> elements = [];
+
+            foreach (var num in nums)
+            {
+                if (elements.Contains(num))
+                    return true;
+
+                elements.Add(num);
+            }
+           
+            return false;
+        }
+    }
+}
--- LogicLeague/Program.cs
+++ LogicLeague/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             var profit = BuySellStock.MaxProfit([7, 1, 5, 3, 6, 4]);
+
+            var contains = FindDuplicate.ContainsDuplicate([1, 2, 3, 1]);
             Console.WriteLine("Hello, World!");
         }
     }
