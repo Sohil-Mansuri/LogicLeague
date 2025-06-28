d18ffeb4280132ff8bc8a762db9f1b2dbfb6e80d
Sohil007 <sohil.mansuri@in.musafir.com>
2025-06-28 15:25:52 +0530

    contains nearyby problem

--- LogicLeague/FindDuplicate.cs
+++ LogicLeague/FindDuplicate.cs
@@ -1,4 +1,6 @@
-﻿namespace LogicLeague
+﻿using System;
+
+namespace LogicLeague
 {
     /// <summary>
     /// https://leetcode.com/problems/contains-duplicate/
@@ -16,7 +18,58 @@
 
                 elements.Add(num);
             }
-           
+
+            return false;
+        }
+
+
+        /// <summary>
+        /// https://leetcode.com/problems/contains-duplicate-ii/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        /// This one is faster because we are not removing here, just lookup and update
+        public static bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            Dictionary<int, int> elements = [];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (elements.ContainsKey(nums[i]))
+                {
+                    if (Math.Abs(i - elements[nums[i]]) <= k)
+                    {
+                        return true;
+                    }
+
+                    elements[nums[i]] = i;
+                }
+                else
+                {
+                    elements[nums[i]] = i;
+                }
+            }
+
+
+            return false;
+        }
+
+        // Sliding window concept
+        public static bool ContainsNearbyDuplicateV2(int[] nums, int k)
+        {
+            HashSet<int> elements = [];
+
+            for (int index = 0; index < nums.Length; index++)
+            {
+                if (elements.Contains(nums[index])) return true;
+
+                elements.Add(nums[index]);
+
+                if (elements.Count > k)
+                    elements.Remove(nums[index - k]);
+            }
+
             return false;
         }
     }
--- LogicLeague/Program.cs
+++ LogicLeague/Program.cs
@@ -7,6 +7,10 @@
             var profit = BuySellStock.MaxProfit([7, 1, 5, 3, 6, 4]);
 
             var contains = FindDuplicate.ContainsDuplicate([1, 2, 3, 1]);
+
+            var contains2 = FindDuplicate.ContainsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2);
+            var contains3 = FindDuplicate.ContainsNearbyDuplicateV2([1, 0, 1, 1], 1);
+            var contains4 = FindDuplicate.ContainsNearbyDuplicateV2([99, 99], 1);
             Console.WriteLine("Hello, World!");
         }
     }
