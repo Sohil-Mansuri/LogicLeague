24b0e70ec55f67d615523cf13d0ab8d1540d8f65
Sohil007 <sohil.mansuri@in.musafir.com>
2025-06-28 23:04:00 +0530

    product of array

---
+++ LogicLeague/ProductOfArrary.cs
@@ -0,0 +1,91 @@
+﻿using System.Dynamic;
+
+namespace LogicLeague
+{
+    /// <summary>
+    /// https://leetcode.com/problems/product-of-array-except-self/
+    /// without division
+    /// </summary>
+    internal class ProductOfArrary
+    {
+        // T(n), S(3n)
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+            int[] prefixProduct = new int[nums.Length];
+            int[] postFixProduct = new int[nums.Length];
+
+            int[] result = new int[nums.Length];
+
+            for (int index = 0; index < nums.Length; index++)
+            {
+                if (index == 0)
+                {
+                    prefixProduct[index] = 1;
+                }
+                else
+                {
+                    prefixProduct[index] = nums[index - 1] * prefixProduct[index - 1];
+                }
+            }
+
+            for (int index = nums.Length - 1; index > -1; index--)
+            {
+                if (index == nums.Length - 1)
+                {
+                    postFixProduct[index] = 1;
+                }
+                else
+                {
+                    postFixProduct[index] = nums[index + 1] * postFixProduct[index + 1];
+                }
+            }
+
+
+            for (int index = 0; index < result.Length; index++)
+            {
+                result[index] = prefixProduct[index] * postFixProduct[index];
+            }
+
+            return result;
+        }
+
+        // Optimal in terms of Space complexity S(n) = O(1), just need 2 extra variables
+        public static int[] ProductExceptSelfV2(int[] nums)
+        {
+            int prefixMul = 1;
+            int postfixMul = 1;
+            int[] result = new int[nums.Length];
+
+            for (int index = 0; index < nums.Length; index++)
+            {
+                if (index == 0)
+                {
+                    prefixMul = 1;
+                }
+                else
+                {
+                    prefixMul = nums[index - 1] * prefixMul;
+                }
+
+                result[index] = prefixMul;
+            }
+
+
+            for (int index = nums.Length - 1; index > -1; index--)
+            {
+                if (index == nums.Length - 1)
+                {
+                    postfixMul = 1;
+                }
+                else
+                {
+                    postfixMul = nums[index + 1] * postfixMul;
+                }
+
+                result[index] = postfixMul * result[index];
+            }
+
+            return result;
+        }
+    }
+}
--- LogicLeague/Program.cs
+++ LogicLeague/Program.cs
@@ -11,6 +11,11 @@
             var contains2 = FindDuplicate.ContainsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2);
             var contains3 = FindDuplicate.ContainsNearbyDuplicateV2([1, 0, 1, 1], 1);
             var contains4 = FindDuplicate.ContainsNearbyDuplicateV2([99, 99], 1);
+
+
+            var productArray = ProductOfArrary.ProductExceptSelf([1, 2, 3, 4]);
+            var productArray2 = ProductOfArrary.ProductExceptSelfV2([2, 3, 5, 7]);
+            var productArray3 = ProductOfArrary.ProductExceptSelfV2([-1, 1, 0, -3, 3]);
             Console.WriteLine("Hello, World!");
         }
     }
