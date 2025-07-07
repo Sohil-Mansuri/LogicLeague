da4550897b67f1a498c67be645cec45e121347ad
Sohil007 <sohil.mansuri@in.musafir.com>
2025-07-07 10:49:01 +0530

    valid sudoko

---
+++ LogicLeague/ValidSudokoProblem.cs
@@ -0,0 +1,48 @@
+﻿namespace LogicLeague
+{
+    /// <summary>
+    /// https://leetcode.com/problems/valid-sudoku/
+    /// </summary>
+    internal class ValidSudokoProblem
+    {
+        public static bool IsValidSudoku(string[][] board)
+        {
+            int size = 9;
+
+            HashSet<string>[] rows = new HashSet<string>[size];
+            HashSet<string>[] columns = new HashSet<string>[size];
+            HashSet<string>[] boxes = new HashSet<string>[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                rows[i] = [];
+                columns[i] = [];
+                boxes[i] = [];
+            }
+
+            for (int row = 0; row < board.Length; row++)
+            {
+                for (int column = 0; column < board.Length; column++)
+                {
+                    if (board[row][column] == ".") continue;
+
+                    if (rows[row].Contains(board[row][column])) return false;
+
+                    rows[row].Add(board[row][column]);
+
+                    if (columns[column].Contains(board[row][column])) return false;
+
+                    columns[column].Add(board[row][column]);
+
+                    int boxIndex = (row / 3) * 3 + (column / 3);
+
+                    if (boxes[boxIndex].Contains(board[row][column])) return false;
+
+                    boxes[boxIndex].Add(board[row][column]);
+                }
+            }
+
+            return true;
+        }
+    }
+}
