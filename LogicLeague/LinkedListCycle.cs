e96ba44d3adf6fe9bb1097892c759f7f96e41f06
Sohil007 <sohil.mansuri@in.musafir.com>
2025-07-26 12:37:26 +0530

    Linked list cycle

---
+++ LogicLeague/LinkedListCycle.cs
@@ -0,0 +1,26 @@
+﻿namespace LogicLeague
+{
+    /// <summary>
+    /// https://leetcode.com/problems/linked-list-cycle/
+    /// </summary>
+    internal class LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null) return false;
+
+            var slow = head;
+            var fast = head.next;
+
+            while(slow != fast)
+            {
+                if (fast == null || fast.next == null) return false;
+
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return true;
+        }
+    }
+}
