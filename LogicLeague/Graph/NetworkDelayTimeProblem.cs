6b13c79fe0a1cf3eb408aaec027447ed73026ada
Sohil007 <sohil.mansuri@in.musafir.com>
2025-10-02 09:53:55 +0530

    Network delay time

---
+++ LogicLeague/Graph/NetworkDelayTimeProblem.cs
@@ -0,0 +1,65 @@
+﻿namespace LogicLeague.Graph
+{
+    /// <summary>
+    /// https://leetcode.com/problems/network-delay-time/description/
+    /// </summary>
+    internal class NetworkDelayTimeProblem
+    {
+        public static int NetworkDelayTime(int[][] times, int n, int k)
+        {
+            Dictionary<int, List<(int nextNode, int nextDistance)>> graph = [];
+
+            foreach (var item in times)
+            {
+                if (!graph.TryGetValue(item[0], out var neighbour))
+                    neighbour = [];
+
+                neighbour.Add((item[1], item[2]));
+                graph[item[0]] = neighbour;
+            }
+
+            var distance = new int[n + 1];
+            for (int i = 0; i < distance.Length; i++)
+            {
+                distance[i] = int.MaxValue;
+            }
+
+            distance[k] = 0;
+            PriorityQueue<int[], int> minHeap = new();
+            minHeap.Enqueue([k, 0], 0);
+
+            while (minHeap.Count > 0)
+            {
+                var element = minHeap.Dequeue();
+                var currentNode = element[0];
+                var currentDistance = element[1];
+
+                if (currentDistance > distance[currentNode])
+                    continue;
+
+                if (graph.TryGetValue(currentNode, out var neighbours))
+                {
+                    foreach (var neighbour in neighbours)
+                    {
+                        var nextNode = neighbour.nextNode;
+                        var nextDistance = currentDistance + neighbour.nextDistance;
+                        if (nextDistance < distance[nextNode])
+                        {
+                            distance[nextNode] = nextDistance;
+                            minHeap.Enqueue([nextNode, nextDistance], nextDistance);
+                        }
+                    }
+                }
+            }
+
+            int minTime = -1;
+            for (int i = 1; i < distance.Length; i++)
+            {
+                if (distance[i] == int.MaxValue) return -1;
+                minTime = Math.Max(minTime, distance[i]);
+            }
+
+            return minTime;
+        }
+    }
+}
--- LogicLeague/Program.cs
+++ LogicLeague/Program.cs
@@ -307,7 +307,7 @@ namespace LogicLeague
             var insertIntervals = InsertInterval.Insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]);
             var insertIntervals2 = InsertInterval.Insert([[1, 2], [3, 6], [7, 9], [10, 11], [12, 16]], [4, 5]);
             var insertIntervals4 = InsertInterval.Insert([], [4, 5]);
-            var insertIntervals5 = InsertInterval.Insert([[1, 3],[6,9]], [4, 5]);
+            var insertIntervals5 = InsertInterval.Insert([[1, 3], [6, 9]], [4, 5]);
 
             var nonOverlapping = NonOverlappingIntervals.EraseOverlapIntervals([[1, 2], [1, 2], [1, 2]]);
             var nonOverlapping2 = NonOverlappingIntervals.EraseOverlapIntervals([[1, 2], [2, 3], [3, 4], [1, 3]]);
@@ -781,19 +781,22 @@ namespace LogicLeague
 
 
             var maxArea = MaxAreaOfIslandProblem.MaxAreaOfIsland
-                                                ([[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], 
-                                                  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], 
-                                                  [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], 
-                                                  [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0], 
-                                                  [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], 
-                                                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], 
-                                                  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], 
+                                                ([[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                                                  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+                                                  [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                                  [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+                                                  [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+                                                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                                                  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
                                                   [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]);
 
             var maxArea1 = MaxAreaOfIslandProblem.MaxAreaOfIsland([[1, 1, 0, 0, 0],
                                                                     [1, 1, 0, 0, 0],
                                                                     [0, 0, 0, 1, 1],
                                                                     [0, 0, 0, 1, 1]]);
+
+            var maxDistance = NetworkDelayTimeProblem.NetworkDelayTime([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2);
+            var maxDistance2 = NetworkDelayTimeProblem.NetworkDelayTime([[1, 2, 1]], 2, 2);
 
             Console.WriteLine("Hello world");
         }
