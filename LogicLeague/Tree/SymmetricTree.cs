2320e1f5e12d5388cd30c00c2e039d0d175c9aef
Sohil007 <sohil.mansuri@in.musafir.com>
2026-03-09 14:59:24 +0530

    Symmetric tree

---
+++ LogicLeague/Tree/SymmetricTree.cs
@@ -0,0 +1,20 @@
+﻿namespace LogicLeague.Tree
+{
+    internal class SymmetricTree
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root is null) return true;
+            return IsSymmetric(root.left, root.right);
+        }
+
+        public bool IsSymmetric(TreeNode left, TreeNode right)
+        {
+            if (left is null && right is null) return true;
+            if (left is null || right is null) return false;
+            return left.val == right.val &&
+            IsSymmetric(left.left, right.right) &&
+            IsSymmetric(left.right, right.left);
+        }
+    }
+}
