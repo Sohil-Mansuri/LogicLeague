df5378460f02e5c8223918dd6938b78ce52020fd
Sohil007 <sohil.mansuri@in.musafir.com>
2026-03-12 16:43:34 +0530

    Construct binary tree using preorder and inorder

---
+++ LogicLeague/Tree/BinaryTreeZigZagOrderTraversal.cs
@@ -0,0 +1,42 @@
+﻿namespace LogicLeague.Tree
+{
+    /// <summary>
+    /// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+    /// </summary>
+    internal class BinaryTreeZigZagOrderTraversal
+    {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            if (root is null) return [];
+            IList<IList<int>> result = [];
+            Queue<TreeNode> queue = new();
+            queue.Enqueue(root);
+
+            bool isLeftToRight = true;
+            while (queue.Count > 0)
+            {
+                var length = queue.Count;
+
+                LinkedList<int> level = new();
+                for (int i = 0; i < length; i++)
+                {
+                    var node = queue.Dequeue();
+
+                    if (isLeftToRight)
+                        level.AddLast(node.val);
+                    else
+                        level.AddFirst(node.val);
+
+                    if (node.left is not null) queue.Enqueue(node.left);
+                    if (node.right is not null) queue.Enqueue(node.right);
+                }
+
+                result.Add(level.ToList());
+                isLeftToRight = !isLeftToRight;
+            }
+
+
+            return result;
+        }
+    }
+}
---
+++ LogicLeague/Tree/ConstructBinaryTree.cs
@@ -0,0 +1,38 @@
+﻿
+namespace LogicLeague.Tree
+{
+    /// <summary>
+    /// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+    /// </summary>
+    internal class ConstructBinaryTree
+    {
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            Dictionary<int, int> map = [];
+
+            for (int i = 0; i < inorder.Length; i++)
+            {
+                map[inorder[i]] = i;
+            }
+
+            int preIndex = 0;
+
+            return BuildTree(preorder, 0, inorder.Length - 1, map, ref preIndex);
+        }
+
+        private TreeNode BuildTree(int[] preorder, int left, int right, Dictionary<int, int> map, ref int preIndex)
+        {
+            if (left > right) return null;
+
+            var rootValue = preorder[preIndex++];
+            var root = new TreeNode { val = rootValue };
+
+            int mid = map[rootValue];
+
+            root.left = BuildTree(preorder, left, mid - 1, map, ref preIndex);
+            root.right = BuildTree(preorder, mid + 1, right, map, ref preIndex);
+
+            return root;
+        }
+    }
+}
