39b8a7bf64779062379d76927093878643191a45
Sohil007 <sohil.mansuri@in.musafir.com>
2026-03-13 08:50:16 +0530

    Kth smallest value in BST

---
+++ LogicLeague/Tree/ConstructBinaryTreeV2.cs
@@ -0,0 +1,38 @@
+﻿
+namespace LogicLeague.Tree
+{
+    /// <summary>
+    /// https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+    /// </summary>
+    internal class ConstructBinaryTreeV2
+    {
+        public TreeNode BuildTree(int[] inorder, int[] postorder)
+        {
+            Dictionary<int, int> map = [];
+
+            for (int i = 0; i < inorder.Length; i++)
+            {
+                map[inorder[i]] = i;
+            }
+
+            int postIndex = postorder.Length - 1;
+
+            return BuildTree(postorder, 0, inorder.Length - 1, ref postIndex, map);
+        }
+
+        private TreeNode BuildTree(int[] postorder, int left, int right, ref int postIndex, Dictionary<int, int> map)
+        {
+            if (left > right) return null;
+
+            var rootValue = postorder[postIndex--];
+            var root = new TreeNode { val = rootValue };
+
+            int mid = map[rootValue];
+
+            root.right = BuildTree(postorder, mid + 1, right, ref postIndex, map);
+            root.left = BuildTree(postorder, left, mid - 1, ref postIndex, map);
+
+            return root;
+        }
+    }
+}
---
+++ LogicLeague/Tree/KthSmallestValue.cs
@@ -0,0 +1,33 @@
+﻿
+namespace LogicLeague.Tree
+{
+    internal class KthSmallestValue
+    {
+        public int KthSmallest(TreeNode root, int k)
+        {
+            if (root is null) return -1;
+
+            List<int> result = [];
+
+            DFS(root, result);
+
+            return result[k - 1];
+        }
+
+        private void DFS(TreeNode node, List<int> result)
+        {
+            if (node.left is null && node.right is null)
+            {
+                result.Add(node.val);
+                return;
+            }
+
+            if (node.left is not null) DFS(node.left, result);
+
+            result.Add(node.val);
+
+            if (node.right is not null) DFS(node.right, result);
+
+        }
+    }
+}
